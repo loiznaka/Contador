e60236506256c5d624dcd816c720acc17a9e5b8e
Luis Marcelo Costa <luismc9915@gmail.com>
2022-06-05 20:33:34 -0300

    Valores negativos
    Adicionado a alteração de cor para vermelho, se valor for negativo.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,17 @@ const CURRENT_NUMBER = document.getElementById('currentNumber');
 function increment() {
     count++;
     CURRENT_NUMBER.innerHTML = count ;
+
+    if(count >= 0) {
+        CURRENT_NUMBER.style.color='white';
+    }
 }
 
 function decrement() {
     count--;
     CURRENT_NUMBER.innerHTML = count ;
+
+    if(count < 0) {
+        CURRENT_NUMBER.style.color='#8A0808';
+    }
 }
