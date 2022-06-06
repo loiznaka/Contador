93e89aae28c8d0526cab9fdd605b4fb7f539cc9c
Luis Marcelo Costa <luismc9915@gmail.com>
2022-06-05 21:39:31 -0300

    Limitador update
    Adicionado bloqueio de botão quando atinge limite de -10 ou 10.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,13 @@ function increment() {
         CURRENT_NUMBER.style.color='white';
     }
     // Limitador
-    if(count == 10){
-        limit_response.innerHTML = "Você atingiu o limite. Não pode mais adicionar !";
-    } else {
-        limit_response.innerHTML = ""
+    if( count == 10){
+        document.getElementById("LimitResponse").innerHTML = "Você atingiu o limite. Não pode mais adicionar !";
+        document.getElementById("adicao").disabled = true;
     }
 }
+
+
 
 function decrement() {
     count--;
@@ -23,5 +24,10 @@ function decrement() {
 
     if(count < 0) {
         CURRENT_NUMBER.style.color='#8A0808';
+    }
+    // Limitador
+    if( count == -10){
+        document.getElementById("LimitResponse").innerHTML = "Você atingiu o limite. Não pode mais remover !";
+        document.getElementById("subtracao").disabled = true;
     }
 }
