ed413af574ac83b7976be969c24f84fd427afbaf
Luis Marcelo Costa <luismc9915@gmail.com>
2022-06-05 22:13:54 -0300

    Correção de Bugs
    Solucionado problema do texto estático quando atingido o limite.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
+// Seta o contador como 0
 let count = 0;
 
+// Variáveis
 const CURRENT_NUMBER = document.getElementById('currentNumber');
 
 function increment() {
     count++;
     CURRENT_NUMBER.innerHTML = count ;
 
+    // Alteração de Cor se Positivo.
     if(count >= 0) {
         CURRENT_NUMBER.style.color='white';
     }
@@ -13,15 +16,17 @@ function increment() {
     if( count == 10){
         document.getElementById("LimitResponse").innerHTML = "Você atingiu o limite. Não pode mais adicionar !";
         document.getElementById("adicao").disabled = true;
+    } else {
+        document.getElementById("LimitResponse").innerHTML = "";
+        document.getElementById("subtracao").disabled = false;
     }
 }
-
-
 
 function decrement() {
     count--;
     CURRENT_NUMBER.innerHTML = count ;
 
+    // Alteração de Cor se Negativo.
     if(count < 0) {
         CURRENT_NUMBER.style.color='#8A0808';
     }
@@ -29,5 +34,8 @@ function decrement() {
     if( count == -10){
         document.getElementById("LimitResponse").innerHTML = "Você atingiu o limite. Não pode mais remover !";
         document.getElementById("subtracao").disabled = true;
+    } else {
+        document.getElementById("LimitResponse").innerHTML = "";
+        document.getElementById("adicao").disabled = false;
     }
 }
