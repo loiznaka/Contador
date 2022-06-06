4f212bf50e6c7c9ec706caea656672a0c40c32b8
Luis Marcelo Costa <luismc9915@gmail.com>
2022-06-05 21:00:41 -0300

    Texto Limitador
    Adicionado resposta quando atingido valor 10.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,12 @@ function increment() {
     if(count >= 0) {
         CURRENT_NUMBER.style.color='white';
     }
+    // Limitador
+    if(count == 10){
+        limit_response.innerHTML = "Você atingiu o limite. Não pode mais adicionar !";
+    } else {
+        limit_response.innerHTML = ""
+    }
 }
 
 function decrement() {
